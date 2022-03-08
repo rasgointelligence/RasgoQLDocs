8e26b6a7e76309d3248636f8bae92f7fc2b71bea
Alex Wolf <alex.r.wolf22@gmail.com>
2022-03-08 17:52:33 -0600

    Script for syncing Transform Docs; Updated Summary.md to no organize by column/row/table

---
+++ sync_transform_docs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Bash Script to sync Transform Documentatiom from the RasgoTransforms repo
+#
+# The github repo for the RaghoTransforms MUST be in the same directory as this repo
+
+# Loop through transform doc file in the RasgoTransforms repo
+transform_doc_file_names=`ls ../RasgoTransforms/docs/*.md`
+echo 1
+for transform_docs_file in $transform_doc_file_names; do
+    file_path_parts=(${transform_docs_file//\// })
+    md_file_name=${file_path_parts[3]}
+
+    # Copy UDT Docs to gitbook repo
+    docs_file_location="./transforms/$md_file_name"
+    echo "Copying UDT doc to $docs_file_location"
+    cp $transform_docs_file $docs_file_location
+done
